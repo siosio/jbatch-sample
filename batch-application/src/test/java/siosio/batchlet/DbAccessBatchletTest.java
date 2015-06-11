34e20e8cf2ca3e811a584b0d272ef4553f339653
siosio <siosio12@gmail.com>
2015-06-11 23:17:50 +0900

    余計なimportを削除

--- batch-application/src/test/java/siosio/batchlet/DbAccessBatchletTest.java
+++ batch-application/src/test/java/siosio/batchlet/DbAccessBatchletTest.java
@@ -18,7 +18,6 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import siosio.TestHelper;
 import siosio.entity.UserEntity;
